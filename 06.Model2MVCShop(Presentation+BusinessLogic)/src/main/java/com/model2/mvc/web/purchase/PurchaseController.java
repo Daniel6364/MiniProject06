2ee5c4d43b5ad3c30e2544918b37000d153048a1
Daniel6364 <iamsung33@naver.com>
2017-11-26 11:37:21 +0900

    bitcamp/dev : modify purchaseList refactoring06
    - 171126

--- 06.Model2MVCShop(Presentation+BusinessLogic)/src/main/java/com/model2/mvc/web/purchase/PurchaseController.java
+++ 06.Model2MVCShop(Presentation+BusinessLogic)/src/main/java/com/model2/mvc/web/purchase/PurchaseController.java
@@ -13,15 +13,17 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.model2.mvc.common.Page;
 import com.model2.mvc.common.Search;
 import com.model2.mvc.service.domain.Purchase;
+import com.model2.mvc.service.domain.User;
 import com.model2.mvc.service.purchase.PurchaseService;
 
 
 //==> 회원관리 Controller
-//@Controller
+@Controller
 public class PurchaseController {
 	
 	///Field
@@ -72,10 +74,10 @@ public class PurchaseController {
 		// Model 과 View 연결
 		model.addAttribute("purchase", purchase);
 		
-		return "forward:/pruchase/getPurchase.jsp";
+		return "forward:/purchase/getPurchase.jsp";
 	}
 	
-	@RequestMapping("/getPurchase.do")
+	//@RequestMapping("/getPurchase.do")
 	public String getPurchase2( @RequestParam("prodNo") String prodNo , Model model ) throws Exception {
 		
 		System.out.println("/getPurchase.do");
@@ -84,15 +86,15 @@ public class PurchaseController {
 		// Model 과 View 연결
 		model.addAttribute("purchase", purchase);
 		
-		return "forward:/pruchase/getPurchase.jsp";
+		return "forward:/purchase/getPurchase.jsp";
 	}
 	
 	@RequestMapping("/updatePurchaseView.do")
-	public String updatePurchaseView( @RequestParam("prodNo") String prodNo , Model model ) throws Exception{
+	public String updatePurchaseView( @RequestParam("tranNo") String tranNo , Model model ) throws Exception{
 
 		System.out.println("/updatePurchaseView.do");
 		//Business Logic
-		Purchase purchase = purchaseService.getPurchase2(Integer.parseInt(prodNo));
+		Purchase purchase = purchaseService.getPurchase2(Integer.parseInt(tranNo));
 		// Model 과 View 연결
 		model.addAttribute("purchase", purchase);
 		
@@ -100,86 +102,41 @@ public class PurchaseController {
 	}
 	
 	@RequestMapping("/updatePurchase.do")
-	public String updatePurchase( @ModelAttribute("purchase") Purchase purchase, Model model , HttpSession session) throws Exception{
+	public String updatePurchase( @ModelAttribute("purchase") Purchase purchase, Model model) throws Exception{
 
-		System.out.println("/updateUser.do");
+		System.out.println("/updatePurchase.do");
 		//Business Logic
-		userService.updateUser(user);
-		
-		String sessionId=((User)session.getAttribute("user")).getUserId();
-		if(sessionId.equals(user.getUserId())){
-			session.setAttribute("user", user);
-		}
-		
-		return "redirect:/getUser.do?userId="+user.getUserId();
-	}
-	
-	@RequestMapping("/loginView.do")
-	public String loginView() throws Exception{
-		
-		System.out.println("/loginView.do");
+		purchaseService.updatePurchase(purchase);
 
-		return "redirect:/user/loginView.jsp";
+		model.addAttribute("purchase", purchase);
+		
+		return "forward:/purchase/updatePurchase.jsp";
 	}
 	
-	@RequestMapping("/login.do")
-	public String login(@ModelAttribute("user") User user , HttpSession session ) throws Exception{
+	@RequestMapping("/listPurchase.do")
+	public String listPurchase( @ModelAttribute("search") Search search, 
+			HttpSession session, Model model ) throws Exception{
 		
-		System.out.println("/login.do");
-		//Business Logic
-		User dbUser=userService.getUser(user.getUserId());
-		
-		if( user.getPassword().equals(dbUser.getPassword())){
-			session.setAttribute("user", dbUser);
-		}
-		
-		return "redirect:/index.jsp";
-	}
-	
-	@RequestMapping("/logout.do")
-	public String logout(HttpSession session ) throws Exception{
-		
-		System.out.println("/logout.do");
-		
-		session.invalidate();
-		
-		return "redirect:/index.jsp";
-	}
-	
-	@RequestMapping("/checkDuplication.do")
-	public String checkDuplication( @RequestParam("userId") String userId , Model model ) throws Exception{
-		
-		System.out.println("/checkDuplication.do");
-		//Business Logic
-		boolean result=userService.checkDuplication(userId);
-		// Model 과 View 연결
-		model.addAttribute("result", new Boolean(result));
-		model.addAttribute("userId", userId);
-
-		return "forward:/user/checkDuplication.jsp";
-	}
-	
-	@RequestMapping("/listUser.do")
-	public String listUser( @ModelAttribute("search") Search search , Model model , HttpServletRequest request) throws Exception{
-		
-		System.out.println("/listUser.do");
+		System.out.println("/listPurchase.do");
 		
 		if(search.getCurrentPage() ==0 ){
 			search.setCurrentPage(1);
 		}
 		search.setPageSize(pageSize);
 		
+		User buyerId = (User) session.getAttribute("user");
+		
 		// Business logic 수행
-		Map<String , Object> map=userService.getUserList(search);
+		Map<String, Object> map = purchaseService.getPurchaseList(search, buyerId.getUserId());
 		
 		Page resultPage = new Page( search.getCurrentPage(), ((Integer)map.get("totalCount")).intValue(), pageUnit, pageSize);
-		System.out.println(resultPage);
+		System.out.println("[###]"+resultPage);
 		
 		// Model 과 View 연결
 		model.addAttribute("list", map.get("list"));
 		model.addAttribute("resultPage", resultPage);
 		model.addAttribute("search", search);
 		
-		return "forward:/user/listUser.jsp";
+		return "forward:/purchase/listPurchase.jsp";
 	}
 }
